7d5be902665fca8aa5ed73e22a40ca77bb665311
serhat yılmaz <srhtylmz19@gmail.com>
2019-04-17 12:03:20 +0300

    Master --> Cool Animated Progress Bar

--- progressBarAnimation/ViewController.swift
+++ progressBarAnimation/ViewController.swift
@@ -1,9 +1,9 @@
 //
-//  ViewController.swift
-//  CircularLoaderLBTA
+//  UIView-Extension.swift
+//  progressBarAnimation
 //
-//  Created by Brian Voong on 12/8/17.
-//  Copyright © 2017 Lets Build That App. All rights reserved.
+//  Created by Serhat Yilmaz on 16.04.2019.
+//  Copyright © 2019 Serhat Yilmaz. All rights reserved.
 //
 
 import UIKit
@@ -24,7 +24,7 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         let layer = CAShapeLayer()
         let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         layer.path = circularPath.cgPath
-        layer.strokeColor = UIColor.rgb(r: 234, g: 46, b: 111).cgColor
+        layer.strokeColor = UIColor.rgb(red: 234, green: 46, blue: 111).cgColor
         layer.lineWidth = 15
         layer.fillColor = UIColor.clear.cgColor
         layer.lineCap = CAShapeLayerLineCap.round
@@ -36,9 +36,9 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         let layer = CAShapeLayer()
         let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         layer.path = circularPath.cgPath
-        layer.strokeColor = UIColor.rgb(r: 56, g: 25, b: 49).cgColor
+        layer.strokeColor = UIColor.rgb(red: 56, green: 25, blue: 49).cgColor
         layer.lineWidth = 20
-        layer.fillColor = UIColor.rgb(r: 21, g: 22, b: 33).cgColor
+        layer.fillColor = UIColor.rgb(red: 21, green: 22, blue: 33).cgColor
         layer.lineCap = CAShapeLayerLineCap.round
         return layer
     }()
@@ -59,27 +59,44 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         layer.path = circularPath.cgPath
         layer.strokeColor = UIColor.clear.cgColor
         layer.lineWidth = 15
-        layer.fillColor = UIColor.rgb(r: 86, g: 30, b: 63).cgColor
+        layer.fillColor = UIColor.rgb(red: 86, green: 30, blue: 63).cgColor
         layer.lineCap = CAShapeLayerLineCap.round
         return layer
     }()
 
+    
+    lazy var startButton: UILabel = {
+        let label = UILabel()
+        label.text = "Start"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 22)
+        label.textColor = .white
+        label.backgroundColor = UIColor.rgb(red: 86, green: 30, blue: 63)
+        label.layer.cornerRadius = 10
+        label.clipsToBounds = true
+        label.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        label.isUserInteractionEnabled = true
+        
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
-        view.backgroundColor = UIColor.backgroundColor
-        
+        view.backgroundColor = UIColor.black
         setupAddView()
         setupAnchor()
         setupShadowAnimation()
         
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
     fileprivate func setupAddView(){
         view.layer.addSublayer(pulsatingLayer)
         view.layer.addSublayer(trackLayer)
         view.layer.addSublayer(shapeLayer)
         view.addSubview(percentageLabel)
+        
+        view.addSubview(startButton)
 
     }
     fileprivate  func setupAnchor() {
@@ -88,6 +105,8 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         pulsatingLayer.position = view.center
         percentageLabel.frame = CGRect(x: 0, y: 0, width: 120, height: 150)
         percentageLabel.center = view.center
+        
+        startButton.anchor(top: nil, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor, paddingTop: 0, paddingleft: 20, paddingBottom: 50, paddingRight: 20, width: 0, height: 40, centerX: view.centerXAnchor, centerY: nil)
     }
     
     fileprivate func setupShadowAnimation(){
@@ -96,7 +115,7 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
     
     fileprivate func beginDownloadingFile() {
         shapeLayer.strokeEnd = 0
-        
+
         let configuration = URLSessionConfiguration.default
         let operationQueue = OperationQueue()
         let urlSession = URLSession(configuration: configuration, delegate: self, delegateQueue: operationQueue)
@@ -107,37 +126,37 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
     }
     
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        
         let percentage = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
         changePercentageLabelText(percentage: percentage)
     }
     
     fileprivate func changePercentageLabelText(percentage: CGFloat){
         DispatchQueue.main.async {
+            self.shapeLayer.strokeEnd = percentage
+
             let mutableAttributedString = NSMutableAttributedString()
             var myAttrString2 = NSAttributedString()
             
-            let myString = "\(Int(percentage * 100))% \n"
+            let percentageString = "\(Int(percentage * 100))% \n"
             let myAttribute = [ NSAttributedString.Key.font: UIFont(name: "Chalkduster", size: 24.0)! ]
-            let myAttrString = NSAttributedString(string: myString, attributes: myAttribute)
+            let myAttrString = NSAttributedString(string: percentageString, attributes: myAttribute)
             
             if percentage == 1.0 {
                 let myString2 = "Download Completed.."
                 let myAttribute2 = [ NSAttributedString.Key.font: UIFont(name: "Georgia", size: 10.0)! ]
                 myAttrString2 = NSAttributedString(string: myString2, attributes: myAttribute2)
+                
+                self.startButton.text = "Done.."
             }else {
-                let myString2 = "downloading.."
+                let myString2 = "Downloading.."
                 let myAttribute2 = [ NSAttributedString.Key.font: UIFont(name: "Chalkduster", size: 10.0)! ]
                 myAttrString2 = NSAttributedString(string: myString2, attributes: myAttribute2)
             }
             mutableAttributedString.append(myAttrString)
             mutableAttributedString.append(myAttrString2)
-            
             self.percentageLabel.attributedText = mutableAttributedString
-            
         }
     }
-    
     //MARK : URLSessionDownloadDelegate
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
         print("Finished downloading file")
